19542eedb79dc2404099529ed7c46fc4d0dd89a6
Truong Gia Khiem <adgcuwc@gmail.com>
2026-03-18 06:36:52 +0700

    chia route, controller

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const router = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(morgan('combined'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', engine({
@@ -16,13 +21,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Routes init
+router(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
